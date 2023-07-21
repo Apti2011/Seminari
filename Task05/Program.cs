16d11acae35d3fd6af056c56c780c778581cb82d
Apti <apta07@gmail.com>
2023-07-21 19:18:51 +0300

    Added Задача 5

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа
+//в промежутке от -N до N. Например 4 -> "-4, -3, -2, -1, 0 1 2 3 4"
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i, j;
+
+if(a==a)
+{ 
+for(i=-a; i<=a; i++)
+Console.Write($"{i} ");
+}
+else if(a==-a);
+for(j=a; j<=-a; j++)
+Console.Write($"{j} ");
+
