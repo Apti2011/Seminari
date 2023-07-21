4ad6193fbcb6395470f7b60dbf55f484f5d4cd80
Apti <apta07@gmail.com>
2023-07-21 20:11:45 +0300

    Added Задача07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трехзначное число и на выходе 
+//показывает последнюю цифру этого числа, например 456 -> 6
+
+Console.WriteLine("Введите трехзначное целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 0) 
+{
+  number *= -1;
+}
+
+if (number >= 100 && number <= 999)
+{
+int result = number % 10;
+Console.WriteLine($"Последняя цифра числа: {result}");
+}
+
+else
+{
+  Console.WriteLine("Число не трехзначное");
+}
+
+
+
+
