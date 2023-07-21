5da487aee51d32091b6c32d72cca7c310a6ac9a2
Apti <apta07@gmail.com>
2023-07-21 21:56:49 +0300

    Добавил Задачу008

--- Homework008/Program.cs
+++ Homework008/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все четные числа от 1 до N, например: 5 ->2, 4. 8->2,4,6,8.
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter;
+Console.WriteLine($"Все четные из числа {number}");
+for (counter = 1; counter <= number; counter++) {
+		if (counter % 2 != 0);
+		else if (counter% 2 == 0) 
+    Console.Write(counter);
+    Console.Write(" ");
+	}
