b236bd1e0c71fa938154f82fe99f5c9259d50834
Apti <apta07@gmail.com>
2023-07-30 22:01:36 +0300

    Добавил задачу Homework23

--- Homework23_27.07.2023/Program.cs
+++ Homework23_27.07.2023/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23 Напишите программу которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.
+//Пример  -> 1, 8, 27
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number<1){
+   Console.WriteLine("Некорректные данные");
+    return;
+}
+
+NumberCube(number);
+void NumberCube(int num)
+{
+for(int i = 1; i<=num; i++){
+   int numCube=i*i*i;
+   Console.WriteLine(numCube);
+}
+}
