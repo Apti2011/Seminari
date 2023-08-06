28952d310b6fdf47386749eca1d5283405aa9881
Apti <apta07@gmail.com>
2023-08-06 14:59:54 +0300

    Added Proverka

---
+++ Proverka/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberSum(int num)
+{
+int sum=0;
+while(num !=0){
+int result = num % 10;
+sum=sum+result;
+num=num/10;
+}
+return sum;
+}
+int sum = NumberSum(number);
+Console.WriteLine($sum);
